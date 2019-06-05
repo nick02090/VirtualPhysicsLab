3b2b9a7bcd0622b682e1e7ec2437232c613774bf
Nikola Ćališ <nidjolas.u.b@gmail.com>
2019-06-05 20:16:43 +0200

    Experiment - frontend layout for new/importing experiments

--- VirtualPhysicsLab.Web/ClientApp/src/views/Experiment.vue
+++ VirtualPhysicsLab.Web/ClientApp/src/views/Experiment.vue
@@ -53,6 +53,131 @@
                 </article>
             </div>
         </div>
+
+        <b-modal :active.sync="firstOpen" @close="close" width="700px">
+            <header class="modal-card-head is-flex" style="text-align: left">
+                <p class="modal-card-title">Odabir pokusa</p>
+                <div class="control margin-left" v-if="group == 1">
+                    <b-field>
+                        <b-input
+                            placeholder="Pretraži..."
+                            type="search"
+                            icon-pack="fas"
+                            icon="search"
+                        ></b-input>
+                    </b-field>
+                </div>
+            </header>
+            <section class="modal-card-body">
+                <nav class="breadcrumb is-centered has-bullet-separator" aria-label="breadcrumbs">
+                    <ul>
+                        <li :class="{'is-active': group == 0}">
+                            <a @click="group = 0">
+                                <span class="icon is-small">
+                                    <i class="fas fa-rocket" aria-hidden="true"></i>
+                                </span>
+                                <span>Novi pokus</span>
+                            </a>
+                        </li>
+                        <li :class="{'is-active': group == 1}">
+                            <a @click="group = 1">
+                                <span class="icon is-small">
+                                    <i class="fas fa-book" aria-hidden="true"></i>
+                                </span>
+                                <span>Moji pokusi</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <hr>
+                <div v-if="group == 0">
+                    <div
+                        class="box margin-right"
+                        style="width: 20rem;"
+                        :class="{'selected': selected0 == 0}"
+                        @click="selected = 0"
+                    >
+                        <article class="media">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                    <img src="@/assets/emptyScene.png">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                        <strong>Prazan pokus</strong>
+                                        <br>
+                                        <small>Prazna scena postavljena s podlogom.</small>
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <div v-if="group == 1">
+                    <div
+                        class="box margin-right margin-left"
+                        style="width: 20rem;"
+                        v-for="(experiment, index) in filteredExperiments"
+                        :key="experiment.id"
+                        :class="{'selected': selected1 == index}"
+                        @click="selected1 = index"
+                    >
+                        <article class="media">
+                            <div class="media-left">
+                                <figure class="image is-64x64">
+                                    <img :src="experiment.image">
+                                </figure>
+                            </div>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                        <strong>{{experiment.title}}</strong>
+                                        <br>
+                                        <small>{{experiment.description}}</small>
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div>
+                        <nav
+                            class="pagination is-rounded"
+                            role="navigation"
+                            aria-label="pagination"
+                        >
+                            <a
+                                class="pagination-previous my-pagination-button"
+                                :class="{'disabled': currentPage == 0}"
+                                @click="previous"
+                            >
+                                <span class="icon">
+                                    <i class="fas fa-angle-left"></i>
+                                </span>Prethodna
+                            </a>
+                            <a
+                                class="pagination-next my-pagination-button"
+                                :class="{'disabled': currentPage == pages - 1}"
+                                @click="next"
+                            >
+                                Sljedeća
+                                <span class="icon">
+                                    <i class="fas fa-angle-right"></i>
+                                </span>
+                            </a>
+                        </nav>
+                    </div>
+                </div>
+            </section>
+            <footer class="modal-card-foot is-flex">
+                <b-button
+                    class="button is-success margin-left"
+                    icon-left="check"
+                    @click="close"
+                >Pokreni</b-button>
+            </footer>
+        </b-modal>
     </div>
 </template>
 
@@ -60,24 +185,73 @@
 import colors from "@/helpers/colors.js";
 import babylon from "@/helpers/babylon/babylon.js";
 import Menu from "@/components/experiment/Menu.vue";
-import { mapActions, mapState } from "vuex";
+import { mapActions, mapState, mapGetters } from "vuex";
 
 export default {
     name: "Experiment",
     data() {
         return {
-            isDisabled: false
+            isDisabled: false,
+            firstOpen: true,
+            group: 0,
+            selected0: 0,
+            selected1: 0,
+            currentPage: 0,
+            pages: 0,
+            batchSize: 3,
+            filteredExperiments: [],
+            user: {
+                experiments: [
+                    {
+                        id: 1,
+                        title: "Pomak",
+                        date: new Date(2019, 5, 3),
+                        description:
+                            "Prikaz pomaka tijela u prostoru. Još nešto da nadopunim.",
+                        image:
+                            "https://bulma.io/images/placeholders/256x256.png"
+                    },
+                    {
+                        id: 2,
+                        title: "Pomak",
+                        date: new Date(2019, 4, 30),
+                        description: "Prikaz pomaka tijela u prostoru.",
+                        image:
+                            "https://bulma.io/images/placeholders/256x256.png"
+                    },
+                    {
+                        id: 3,
+                        title: "Pomak",
+                        date: new Date(2019, 4, 25),
+                        description: "Prikaz pomaka tijela u prostoru.",
+                        image:
+                            "https://bulma.io/images/placeholders/256x256.png"
+                    },
+                    {
+                        id: 4,
+                        title: "Pomak",
+                        date: new Date(2019, 4, 25),
+                        description: "Prikaz pomaka tijela u prostoru.",
+                        image:
+                            "https://bulma.io/images/placeholders/256x256.png"
+                    }
+                ]
+            }
         };
     },
     components: {
         Menu
     },
     mounted() {
-        this.initScene();
+        this.pages = Math.ceil(this.user.experiments.length / this.batchSize);
+        this.update();
     },
     computed: {
         ...mapState({
             isPlaying: state => state.experiment.playing
+        }),
+        ...mapGetters({
+            isLoggedIn: "user/isLoggedIn"
         }),
         scene: {
             get: function() {
@@ -92,6 +266,29 @@ export default {
         ...mapActions({
             createWalls: "experiment/createWalls"
         }),
+        previous() {
+            if (this.currentPage > 0) {
+                this.selected1 = 0;
+                this.currentPage--;
+                this.update();
+            }
+        },
+        next() {
+            if (this.currentPage < this.pages - 1) {
+                this.selected1 = 0;
+                this.currentPage++;
+                this.update();
+            }
+        },
+        update() {
+            var end = (this.currentPage + 1) * this.batchSize;
+            var start = end - this.batchSize;
+            this.filteredExperiments = this.user.experiments.slice(start, end);
+        },
+        close() {
+            this.initScene();
+            this.firstOpen = false;
+        },
         unlockPlaying() {
             this.isDisabled = false;
         },
@@ -144,5 +341,20 @@ export default {
 <style scoped>
 #renderCanvas {
     width: 100%;
+}
+.box {
+    cursor: pointer;
+}
+.box.selected {
+    background-color: #f9f9f9;
+    box-shadow: 1px 1px 10px black;
+    z-index: 20;
+}
+.my-pagination-button {
+    text-decoration: none !important;
+}
+.my-pagination-button.disabled {
+    cursor: not-allowed;
+    color: #bfbfbf !important;
 }
 </style>
